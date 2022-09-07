99387b69cd25cf190adc682f2add20a2ebc984c5
victor-tm-souza <victor.souza@ua.pt>
2022-09-08 00:50:29 +0100

    Updates

---
+++ precache-manifest.e31f12bef24436db8738e6e0e1d2afb1.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "6df147b00e52dd2a0cb8",
+    "url": "/static/css/main.aa2137fe.chunk.css"
+  },
+  {
+    "revision": "6df147b00e52dd2a0cb8",
+    "url": "/static/js/main.6df147b0.chunk.js"
+  },
+  {
+    "revision": "1badc261e10a91f18ad1",
+    "url": "/static/css/1.987319db.chunk.css"
+  },
+  {
+    "revision": "1badc261e10a91f18ad1",
+    "url": "/static/js/1.1badc261.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b6d0265589feb40f49bb0dcbf2fdcf07",
+    "url": "/static/media/Web_dev.b6d02655.png"
+  },
+  {
+    "revision": "6031b57a8a02f819d28b275fe28efbab",
+    "url": "/static/media/html.6031b57a.png"
+  },
+  {
+    "revision": "82713691cd06304d62a496dcdb9ba7ce",
+    "url": "/static/media/css.82713691.png"
+  },
+  {
+    "revision": "537700448b18884f24d6ec883cfdc544",
+    "url": "/static/media/javascript.53770044.png"
+  },
+  {
+    "revision": "71f0d08433273895dbe63199cb5b3e05",
+    "url": "/static/media/python.71f0d084.png"
+  },
+  {
+    "revision": "e09b043ff7ffcf5e477fc39ae9e8635e",
+    "url": "/static/media/java.e09b043f.png"
+  },
+  {
+    "revision": "9ce1bba1136cacf4c8670e11e77e60c6",
+    "url": "/static/media/c.9ce1bba1.png"
+  },
+  {
+    "revision": "fc88fc1666dd02c6937ca87dd1e38033",
+    "url": "/static/media/react.fc88fc16.png"
+  },
+  {
+    "revision": "05c5609fb3d92c7271253864f26a13b3",
+    "url": "/static/media/sql.05c5609f.png"
+  },
+  {
+    "revision": "0d36dced42c17a2f9143a07ec89f7312",
+    "url": "/static/media/gns3.0d36dced.png"
+  },
+  {
+    "revision": "4239cb07e808106ef33d8673d913d08a",
+    "url": "/static/media/blob.4239cb07.png"
+  },
+  {
+    "revision": "795ff91ab02a456de322b8796cac0a82",
+    "url": "/static/media/github.795ff91a.png"
+  },
+  {
+    "revision": "88cdfa312954c96daf389276076b32b3",
+    "url": "/static/media/linked.88cdfa31.png"
+  },
+  {
+    "revision": "17159678fee7dd0f59268f80af164055",
+    "url": "/static/media/email.17159678.png"
+  },
+  {
+    "revision": "eb0997e8b018418614834ab6b1edc590",
+    "url": "/static/media/Data_Centric_Machine_Learning_ops_for_Autonomous_Driving_censored.eb0997e8.pdf"
+  },
+  {
+    "revision": "811019a8d788d98e86c3f059fbee8372",
+    "url": "/static/media/cv-eng.811019a8.pdf"
+  },
+  {
+    "revision": "5e2f6338a7cc0cd19237fd25e3b123a4",
+    "url": "/static/media/card_peci.5e2f6338.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "6d28e0cf89af72ea7d44f29d8154abd2",
+    "url": "/static/media/intro-wave.6d28e0cf.svg"
+  },
+  {
+    "revision": "2f5e08c0da0c0367750e1813192e0eb1",
+    "url": "/static/media/layered-waves-haikei-4.2f5e08c0.svg"
+  },
+  {
+    "revision": "0ba0cea21371d65d46623ab321001c92",
+    "url": "/static/media/layered-waves-haikei.0ba0cea2.svg"
+  },
+  {
+    "revision": "d7af4e4f0000a90fa11f57b16856378a",
+    "url": "/static/media/layered-waves-haikei-phone-2.d7af4e4f.svg"
+  },
+  {
+    "revision": "aabca6a993450627a226a4a5268ed8f5",
+    "url": "/static/media/layered-waves-haikei-phone-1.aabca6a9.svg"
+  },
+  {
+    "revision": "1a8bdc2821b31b19623842585bf79537",
+    "url": "/index.html"
+  }
+];
